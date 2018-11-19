4dc2f6d8aac8a4235684b251a52c576d2ea4cf33
Muhammad Irfan <irfan@bukaide.com>
2018-11-19 23:38:46 +0700

    Initial Commit

--- application/controllers/DashboardCtrl.php
+++ application/controllers/DashboardCtrl.php
@@ -3,10 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class DashboardCtrl extends MY_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+
+        //Do your magic here
+    }
 
     public function index()
     {
-
+        $this->render('Dashboard');
     }
 
 }
